e2f0711c694aee6d2d71cdbc1941e462cec1a3a1
rishabhhanday <rishabhhanday.rh@gmail.com>
2021-08-27 11:57:37 +0530

    initial commit

---
+++ src/test/java/com/poc/eks/ElsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
